8c14b546d99d3488d4b3fd595350334bc4827d7c
Remy Moll <mollator2@gmail.com>
2020-12-16 14:16:32 +0100

    even better

--- clock/api/hat/tester.py
+++ clock/api/hat/tester.py
@@ -1,10 +1,13 @@
 import unicorn
 
 led = unicorn.UnicornHat(32,16)
-
+r = 0
+b = 0
 for i in range(16*32):
     x = i % 32
     y = i // 32
-    led.set_pixel(x,y, 10, 200, 30)
+    r += (x % 16 == 0)*5
+    b+= (y % 16 == 0)*5
+    led.set_pixel(x,y, r, 200, b)
     if i%2 == 0:
         led.show()
